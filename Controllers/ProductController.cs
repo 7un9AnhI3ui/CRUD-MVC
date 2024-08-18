ec9d636df13c50f85e29b107de32f6635efa6230
tab <140590830+7un9AnhI3ui@users.noreply.github.com>
2024-08-18 20:39:28 +0800

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using crud.Data;
+using crud.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace crud.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Product.ToList();
+            return View(products);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            _context.Product.Add(product);
+            _context.SaveChanges();
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var product = _context.Product.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Update(product);
+                _context.SaveChanges();
+            }
+            return View(product);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _context.Product.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Product.Remove(product);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
+
---
+++ Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+//using crud.Data;
+//using Microsoft.EntityFrameworkCore;
+
+namespace crud.Models
+{
+    /*public class ProductContext: ApplicationDbContext
+    {
+        public ProductContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }*/
+
+    public class Product
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public string? Color { get; set; }
+        public string? Type{ get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
+
